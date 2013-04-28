a06733067440b0fbd5a180fd7170a20a443f3614
Breku <jakub.brek@gmail.com>
2013-04-28 09:35:35 +0200

    spring mvc

---
+++ src/main/java/com/brekol/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.brekol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * User: Breku
+ * Date: 28.04.13
+ */
+@Controller
+@RequestMapping(value = {"/","index","home"})
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String init(){
+
+        return "home/index";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/page/home/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Breku
+  Date: 28.04.13
+  Time: 09:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>asdfasdf</title>
+</head>
+<body>
+                asdfasdfasdfasdf
+</body>
+</html>
